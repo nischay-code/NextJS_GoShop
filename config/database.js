ddec6e7eed65fb8b4f90de6a9319d64ba01c0dba
nischay-code <nischay.patel.code@gmail.com>
2022-07-16 11:43:30 +0530

    First Commit

---
+++ config/database.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "Nischay"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Nischay@8989"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  },
+});
+
+/*const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
+*/
+
