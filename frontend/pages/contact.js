cfedbe6ef9594c8701984f945c8a2876265aa8eb
nischay-code <nischay.patel.code@gmail.com>
2022-07-20 22:47:49 +0530

    Index Changed

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -21,11 +21,9 @@ const contact = () => {
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Feedback
+              Contact Us
             </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
-            </p>
+
             <div className="relative mb-4">
               <label
                 htmlFor="email"
@@ -45,7 +43,7 @@ const contact = () => {
                 htmlFor="message"
                 className="leading-7 text-sm text-gray-600"
               >
-                Message
+                What issue are you facing?
               </label>
               <textarea
                 id="message"
@@ -54,11 +52,10 @@ const contact = () => {
               ></textarea>
             </div>
             <button className="text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+              Submit
             </button>
             <p className="text-xs text-gray-500 mt-3">
-              Chicharrones blog helvetica normcore iceland tousled brook viral
-              artisan.
+              Want to reach us old style ? Here is our postal address
             </p>
           </div>
         </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,39 +2,45 @@
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
-      <img
-        className=" object-center bg-yellow-300 w-[100vw] h-[70vh] "
-        src="bg.jpg"
-        alt=""
-      />
       <div className="hidden bg-black text-black"></div>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
+        <div className="container px-5 py-2 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h2 className="text-xs text-black tracking-widest font-medium title-font mb-1">
-              ROOF PARTY POLAROID
+              Electronic Commerce Company
             </h2>
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Master Cleanse Reliac Heirloom
+              MyShop
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table. Franzen you probably
-              heard of them man bun deep jianbing selfies heirloom prism food
-              truck ugh squid celiac humblebrag.
+              Online Shopping Site for Mobiles, Electronics, Furniture, Grocery,
+              Lifestyle, Books & More. Best Offers!
             </p>
+            <img
+              className=" object-center bg-yellow-300 w-[100vw] h-[70vh] "
+              src="bg.jpg"
+              alt=""
+            />
           </div>
           <div className="flex flex-wrap">
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                Shooting Stars
+                Top Offer
               </h2>
               <p className="leading-relaxed text-base mb-4">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                doloribus quasi hic repellendus eaque? Maiores vitae libero
+                ratione eos officiis sed maxime debitis voluptatum! Similique,
+                voluptatem maxime fuga molestiae voluptatibus sequi aut harum
+                officiis. Ut hic consectetur consequatur eaque enim illo sequi
+                voluptatem reprehenderit porro vel, asperiores earum laborum
+                tempora quidem! Earum placeat obcaecati dolor nemo repudiandae!
+                Placeat delectus maxime quis eum sint voluptatibus, atque
+                sapiente, porro deleniti tempora adipisci, ducimus commodi
+                assumenda nesciunt accusamus numquam veniam ullam. Eum, ad?
               </p>
               <a className="text-black inline-flex items-center">
-                Learn More
+                Buy Now
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -50,14 +56,22 @@ export default function Home() {
             </div>
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                The Catalyzer
+                Grocery
               </h2>
               <p className="leading-relaxed text-base mb-4">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                doloribus quasi hic repellendus eaque? Maiores vitae libero
+                ratione eos officiis sed maxime debitis voluptatum! Similique,
+                voluptatem maxime fuga molestiae voluptatibus sequi aut harum
+                officiis. Ut hic consectetur consequatur eaque enim illo sequi
+                voluptatem reprehenderit porro vel, asperiores earum laborum
+                tempora quidem! Earum placeat obcaecati dolor nemo repudiandae!
+                Placeat delectus maxime quis eum sint voluptatibus, atque
+                sapiente, porro deleniti tempora adipisci, ducimus commodi
+                assumenda nesciunt accusamus numquam veniam ullam. Eum, ad?
               </p>
               <a className="text-black inline-flex items-center">
-                Learn More
+                Buy Now
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -73,14 +87,22 @@ export default function Home() {
             </div>
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                Neptune
+                Fashion
               </h2>
               <p className="leading-relaxed text-base mb-4">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                doloribus quasi hic repellendus eaque? Maiores vitae libero
+                ratione eos officiis sed maxime debitis voluptatum! Similique,
+                voluptatem maxime fuga molestiae voluptatibus sequi aut harum
+                officiis. Ut hic consectetur consequatur eaque enim illo sequi
+                voluptatem reprehenderit porro vel, asperiores earum laborum
+                tempora quidem! Earum placeat obcaecati dolor nemo repudiandae!
+                Placeat delectus maxime quis eum sint voluptatibus, atque
+                sapiente, porro deleniti tempora adipisci, ducimus commodi
+                assumenda nesciunt accusamus numquam veniam ullam. Eum, ad?
               </p>
               <a className="text-black inline-flex items-center">
-                Learn More
+                Buy Now
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -96,14 +118,22 @@ export default function Home() {
             </div>
             <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                Melanchole
+                Home
               </h2>
               <p className="leading-relaxed text-base mb-4">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                doloribus quasi hic repellendus eaque? Maiores vitae libero
+                ratione eos officiis sed maxime debitis voluptatum! Similique,
+                voluptatem maxime fuga molestiae voluptatibus sequi aut harum
+                officiis. Ut hic consectetur consequatur eaque enim illo sequi
+                voluptatem reprehenderit porro vel, asperiores earum laborum
+                tempora quidem! Earum placeat obcaecati dolor nemo repudiandae!
+                Placeat delectus maxime quis eum sint voluptatibus, atque
+                sapiente, porro deleniti tempora adipisci, ducimus commodi
+                assumenda nesciunt accusamus numquam veniam ullam. Eum, ad?
               </p>
               <a className="text-black inline-flex items-center">
-                Learn More
+                Buy Now
                 <svg
                   fill="none"
                   stroke="currentColor"
